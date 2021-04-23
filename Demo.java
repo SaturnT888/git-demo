bbe9f512cf1bdaf32cd4d81458e224c7f89691d1
Vazhiko <saturn@airpost.net>
2021-04-23 21:53:04 +0300

    commands moved to readme.md

--- Demo.java
+++ Demo.java
@@ -5,19 +5,3 @@ public class Demo {
         System.out.println ("Hallo, Ich bin Java!");
     }
 }
-
-/* git */
-// git config --global user.email "email@email"
-// git config -- global user.name "name"
-// git --version
-// git --help
-// git init
-// git add .
-// git add <file>
-// git rm --cached <file>
-// .gitignore
-// git branch
-// git branch <branch name>
-// git branch -D <branch name>
-// git checkout <branch name>
-// git checkout -b new
--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
-# git and github course
+# git and github demo
 
+// git config --global user.email "email@email"
+// git config -- global user.name "name"
+// git --version
+// git --help
+// git init
+// git add .
+// git add <file>
+// git rm --cached <file>
+// .gitignore
+// git branch
+// git branch <branch name>
+// git branch -D <branch name>
+// git checkout <branch name>
+// git checkout -b new
