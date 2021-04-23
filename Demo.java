8e4bc6ca9b748e8d39484789705bb5236df639e2
Vazhiko <saturn@airpost.net>
2021-04-23 20:54:06 +0300

    init

---
+++ Demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Demo {
+    public static void main (String[] args) {
+        System.out.println ("Hallo, Ich bin Java!");
+    }
+}
+
+/* git */
+// git --version
+// git --help
+// git init
+// git add .
+// git add <file>
+// git rm --cached <file>
